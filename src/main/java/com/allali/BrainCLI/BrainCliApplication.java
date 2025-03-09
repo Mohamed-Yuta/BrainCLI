36b2137129af1444620b5e28e65c9d766370c69a
Mohamed-Yuta <allali.mohamedamine22@gmail.com>
2025-03-09 05:13:59 +0000

    first commit

---
+++ src/main/java/com/allali/BrainCLI/BrainCliApplication.java
@@ -0,0 +1,13 @@
+package com.allali.BrainCLI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainCliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allali/BrainCLI/BrainCliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allali.BrainCLI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainCliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
